2e5a8607261dbdb7b0bf1bd7ca6d68a3f5f26319
Olavur Ellefsen <oe@tokni.com>
2024-01-10 08:48:55 +0000

    fix: Handle contact, organization, tenancy aggregators

--- src/functions/transform.entrypoint.ts
+++ src/functions/transform.entrypoint.ts
@@ -7,13 +7,39 @@
 interface Input<T = any> {
   eventId: string;
   validTime: string;
+  eventType: string;
   payload: T;
 }
 
 export default async function (input: Input) {
-  console.info(`Received event ${input.eventId}, with payload ${JSON.stringify(input.payload)} and valid time ${input.validTime}`);
+  console.info(`Received event ${input.eventId} with payload ${JSON.stringify(input.payload)} and valid time ${input.validTime}`);
+  console.info(`The aggregator is ${input.aggregator}, the event type is ${input.eventType} and the contact has contactid=${input.payload.contactid}`);
+  if (input.aggregator === "contact") {
+    if (input.eventType === "delete") {
+      return {
+        contactid: input.payload.contactid
+      };
+    }
+  }
+  if (input.aggregator === "organization") {
+    if (input.eventType === "delete") {
+      return {
+        organizationid: input.payload.organizationid
+      };
+    }
+  }
+  if (input.aggregator === "tenancy") {
+    if (input.eventType === "delete") {
+      return {
+        tenancyid: input.payload.tenancyid
+      };
+    }
+    if (!input.payload.tenancyid || !input.payload.organizationid || !input.payload.contactid) {
+      return null
+    }
+  }
   return {
-    eventid: input.eventId,
+    eventId: input.eventId,
     validtime: input.validTime,
     ...input.payload,
   };
