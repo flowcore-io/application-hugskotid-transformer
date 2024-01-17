324ad1f70489c8a545510f178f21163e349d6418
Olavur Ellefsen <oe@tokni.com>
2024-01-17 18:25:56 +0000

    fix: change eventID to eventid

--- src/functions/transform.entrypoint.ts
+++ src/functions/transform.entrypoint.ts
@@ -8,38 +8,49 @@ interface Input<T = any> {
   eventId: string;
   validTime: string;
   eventType: string;
+  aggregator: string;
   payload: T;
 }
 
 export default async function (input: Input) {
-  console.info(`Received event ${input.eventId} with payload ${JSON.stringify(input.payload)} and valid time ${input.validTime}`);
-  console.info(`The aggregator is ${input.aggregator}, the event type is ${input.eventType} and the contact has contactid=${input.payload.contactid}`);
+  console.info(
+    `Received event ${input.eventId} with payload ${JSON.stringify(
+      input.payload,
+    )} and valid time ${input.validTime}`,
+  );
+  console.info(
+    `The aggregator is ${input.aggregator}, the event type is ${input.eventType} and the contact has contactid=${input.payload.contactid}`,
+  );
   if (input.aggregator === "contact") {
     if (input.eventType === "delete") {
       return {
-        contactid: input.payload.contactid
+        contactid: input.payload.contactid,
       };
     }
   }
   if (input.aggregator === "organization") {
     if (input.eventType === "delete") {
       return {
-        organizationid: input.payload.organizationid
+        organizationid: input.payload.organizationid,
       };
     }
   }
   if (input.aggregator === "tenancy") {
     if (input.eventType === "delete") {
       return {
-        tenancyid: input.payload.tenancyid
+        tenancyid: input.payload.tenancyid,
       };
     }
-    if (!input.payload.tenancyid || !input.payload.organizationid || !input.payload.contactid) {
-      return null
+    if (
+      !input.payload.tenancyid ||
+      !input.payload.organizationid ||
+      !input.payload.contactid
+    ) {
+      return null;
     }
   }
   return {
-    eventId: input.eventId,
+    eventid: input.eventId,
     validtime: input.validTime,
     ...input.payload,
   };
